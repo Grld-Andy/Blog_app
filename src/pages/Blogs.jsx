d778004a82e5f0d98505fd2f653395a911d2581d
Grld-Andy <andyansong82@gmail.com>
2024-09-25 23:02:54 +0100

    search

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,43 +1,48 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import BlogCell from '../components/BlogCell'
 import Button from '../components/Button'
 import { BlogContext } from '../context/BlogContext'
 import { useNavigate } from 'react-router'
 
 const Blogs = () => {
-    const [tags, setTags] = useState(['sports', 'technology', 'politics', 'agriculture'])
-    const [selectedTag, setSelectedTag] = useState(tags[0])
+    const [query, setQuery] = useState("")
+    const [filteredBlogs, setFilteredBlogs] = useState([])
     const {blogs} = useContext(BlogContext)
     const navigate = useNavigate()
 
     const navigateToCreate = () => {
         navigate('/createBlog')
     }
-  
-    const handleTagClick = (index) => {
-      setSelectedTag(tags[index])
+
+    const handleQueryChange = (e) => {
+        setQuery(e.target.value)
     }
 
+    useEffect(() => {
+        const filtered = blogs.filter(blog => 
+            blog.title.toLowerCase().includes(query.toLowerCase())
+        )
+        setFilteredBlogs(filtered)
+    }, [blogs, query])
+  
     return (
         <div>
-            <div className='tags'>
-                {
-                    tags.map((tag, index) => (
-                    <h3 key={index} onClick={() => handleTagClick(index)}>{tag}</h3>
-                    ))
-                }
-            </div>
+            <form className="inputContainer">
+                <input type='text' onChange={handleQueryChange} value={query} placeholder='Search by title'/>
+            </form>
             <div className='selectedTag'>
                 <h1>Blogs</h1>
                 <Button text="Add +" onClickFunc={navigateToCreate}/>
             </div>
             <div className='blogs'>
                 {
-                    blogs.length ?
-                    blogs.map((blog, index) => (
-                        <BlogCell key={index} blog={blog}/>
-                    ))
-                    : <h2>No blogs posted</h2>
+                    filteredBlogs.length ? (
+                        filteredBlogs.map((blog, index) => (
+                            <BlogCell key={index} blog={blog} />
+                        ))
+                    ) : (
+                        <h2>No blogs found</h2>
+                    )
                 }
             </div>
         </div>
